b15edf49eb03f5be8bf8bfef50bafce7b6b1a53d
Dumitru <cozmiucd.1at@gmail.com>
2025-02-08 21:24:22 +0200

    restyling

--- script.js
+++ script.js
@@ -3,135 +3,163 @@ console.clear()
 const oriImages = {
   1: {
     category: "moon",
-    url: "http://127.0.0.1:3000/Images/20221109_215055.jpg",
+    url: "Images/20221109_215055.jpg",
   },
   2: {
     category: "special light",
-    url: "http://127.0.0.1:3000/Images/20221122_155501.jpg",
+    url: "Images/20221122_155501.jpg",
   },
   3: {
     category: "lamp",
-    url: "http://127.0.0.1:3000/Images/20221124_202624.jpg",
+    url: "Images/20221124_202624.jpg",
   },
   4: {
     category: "traffic",
-    url: "http://127.0.0.1:3000/Images/20230214_084410.jpg",
+    url: "Images/20230214_084410.jpg",
   },
   5: {
     category: "sunset",
-    url: "http://127.0.0.1:3000/Images/20230220_181153.jpg",
+    url: "Images/20230220_181153.jpg",
   },
   6: {
     category: "sunset",
-    url: "http://127.0.0.1:3000/Images/20230307_064012.jpg",
+    url: "Images/20230307_064012.jpg",
   },
   7: {
     category: "casual",
-    url: "http://127.0.0.1:3000/Images/20230307_070157.jpg",
+    url: "Images/20230307_070157.jpg",
   },
   8: {
     category: "moon",
-    url: "http://127.0.0.1:3000/Images/20230307_215043.jpg",
+    url: "Images/20230307_215043.jpg",
   },
   9: {
     category: "monuments",
-    url: "http://127.0.0.1:3000/Images/20230309_143234.jpg",
+    url: "Images/20230309_143234.jpg",
   },
   10: {
     category: "lamp",
-    url: "http://127.0.0.1:3000/Images/20230325_223427.jpg",
+    url: "Images/20230325_223427.jpg",
   },
   11: {
     category: "traffic",
-    url: "http://127.0.0.1:3000/Images/20230404_185950.jpg",
+    url: "Images/20230404_185950.jpg",
   },
   12: {
     category: "sky",
-    url: "http://127.0.0.1:3000/Images/20230406_174623.jpg",
+    url: "Images/20230406_174623.jpg",
   },
   13: {
     category: "monuments",
-    url: "http://127.0.0.1:3000/Images/20230409_121303.jpg",
+    url: "Images/20230409_121303.jpg",
   },
   14: {
     category: "monuments",
-    url: "http://127.0.0.1:3000/Images/20230409_123016.jpg",
+    url: "Images/20230409_123016.jpg",
   },
   15: {
     category: "sky",
-    url: "http://127.0.0.1:3000/Images/20230413_140553.jpg",
+    url: "Images/20230413_140553.jpg",
   },
   16: {
     category: "sky",
-    url: "http://127.0.0.1:3000/Images/20230413_142036.jpg",
+    url: "Images/20230413_142036.jpg",
   },
   17: {
     category: "nature",
-    url: "http://127.0.0.1:3000/Images/20230505_212349.jpg",
+    url: "Images/20230505_212349.jpg",
   },
   18: {
     category: "nature",
-    url: "http://127.0.0.1:3000/Images/20230506_204859.jpg",
+    url: "Images/20230506_204859.jpg",
   },
   19: {
     category: "good view",
-    url: "http://127.0.0.1:3000/Images/20230506_205601.jpg",
+    url: "Images/20230506_205601.jpg",
   },
   20: {
     category: "sky",
-    url: "http://127.0.0.1:3000/Images/20230514_154134.jpg",
+    url: "Images/20230514_154134.jpg",
   },
   21: {
     category: "nature",
-    url: "http://127.0.0.1:3000/Images/20230514_205309.jpg",
+    url: "Images/20230514_205309.jpg",
   },
   22: {
     category: "nature",
-    url: "http://127.0.0.1:3000/Images/20230515_202543.jpg",
+    url: "Images/20230515_202543.jpg",
   },
   23: {
     category: "sky",
-    url: "http://127.0.0.1:3000/Images/20230526_204219.jpg",
+    url: "Images/20230526_204219.jpg",
   },
   24: {
     category: "nature",
-    url: "http://127.0.0.1:3000/Images/20230527_151438.jpg",
+    url: "Images/20230527_151438.jpg",
   },
   25: {
     category: "colorful",
-    url: "http://127.0.0.1:3000/Images/20230601_150558.jpg",
+    url: "Images/20230601_150558.jpg",
   },
   26: {
     category: "nature",
-    url: "http://127.0.0.1:3000/Images/20230601_153745.jpg",
+    url: "Images/20230601_153745.jpg",
   },
   27: {
     category: "love",
-    url: "http://127.0.0.1:3000/Images/20230603_101411.jpg",
+    url: "Images/20230603_101411.jpg",
   },
   28: {
     category: "love",
-    url: "http://127.0.0.1:3000/Images/20230603_101437.jpg",
+    url: "Images/20230603_101437.jpg",
   },
   29: {
     category: "sunrise",
-    url: "http://127.0.0.1:3000/Images/20230622_064034.jpg",
+    url: "Images/20230622_064034.jpg",
   },
   30: {
     category: "nature",
-    url: "http://127.0.0.1:3000/Images/20230628_202232.jpg",
+    url: "Images/20230628_202232.jpg",
   },
   31: {
     category: "casual",
-    url: "http://127.0.0.1:3000/Images/20230727_201136.jpg",
+    url: "Images/20230727_201136.jpg",
   },
   32: {
     category: "good view",
-    url: "http://127.0.0.1:3000/Images/20230730_200544.jpg",
+    url: "Images/20230730_200544.jpg",
   },
   33: {
     category: "night light",
-    url: "http://127.0.0.1:3000/Images/20230730_203124.jpg",
+    url: "Images/20230730_203124.jpg",
+  },
+  34: {
+    category: "night light",
+    url: "Images/20230730_203124_remastered.jpg",
+  },
+  35: {
+    category: "night light",
+    url: "Images/20230730_214605.jpg",
+  },
+  36: {
+    category: "sky",
+    url: "Images/20230731_112052.jpg",
+  },
+  37: {
+    category: "moon",
+    url: "Images/20230801_214152.jpg",
+  },
+  38: {
+    category: "sky",
+    url: "Images/20230810_113149.jpg",
+  },
+  39: {
+    category: "monuments",
+    url: "Images/20230811_140302.jpg",
+  },
+  40: {
+    category: "sunset",
+    url: "Images/20230812_193938.jpg",
   },
 };
 //console.log(oriImages);
@@ -178,7 +206,7 @@ function filterImages(event) {
 
 function renderImages(image, div) {
     const img = document.createElement("img");
-    img.src = image["url"].slice(22);
+    img.src = image["url"];
     img.alt = image["category"];
     div.appendChild(img);
 } //renderImages();
